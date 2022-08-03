5e53ab45ffed5cadfd51616684bdd3e9d55f2541
MohdNaseem82 <iamNaseemuddin@hotmail.com>
2022-08-03 14:46:08 +0200

    added 3 model code

--- model.py
+++ model.py
@@ -1,1 +1,12 @@
 print('adding all  ML related code here')
+
+def model1():
+    print('adding code for the model1')
+
+def model2():
+    print('adding code for the model2')
+
+def model3():
+    print('adding code for the model3')
+
+    
